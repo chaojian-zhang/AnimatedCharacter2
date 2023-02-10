f201ce587b413e76b8b2ceb688f8b047f7aba673
Charles Zhang <szinubuntu@gmail.com>
2023-02-10 17:28:28 -0500

    OpenAI Setup

---
+++ AnimatedCharacter/CompletionHelper.cs
@@ -0,0 +1,42 @@
+﻿using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+namespace AnimatedCharacter
+{
+    public static class CompletionHelpers
+    {
+        public static async Task<string> Complete(string input, string model = null, int maxTokens = 256, float temperature = 0.5f)
+        {
+            OpenAIService OpenAiService = new OpenAIService(new OpenAiOptions()
+            {
+                ApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")
+            });
+            var completionResult = await OpenAiService.Completions.CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = input,
+                Model = model ?? Models.TextDavinciV3,
+                MaxTokens = maxTokens,
+                Temperature = temperature
+            });
+
+            if (completionResult.Successful)
+            {
+                string reply = completionResult.Choices.FirstOrDefault()?.Text;
+                if (!string.IsNullOrEmpty(reply))
+                    return reply.Trim();
+                return null;
+            }
+            else
+            {
+                if (completionResult.Error == null)
+                    throw new Exception("Unknown Error");
+                throw new Exception($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+            }
+        }
+    }
+}
